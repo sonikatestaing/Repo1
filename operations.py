fb047581a47e18b91e293065f1892ee22213fb88
sonika <sonika@testaing.com>
2020-02-20 12:03:40 +0530

    modified operations.py | added index.html

--- operations.py
+++ operations.py
@@ -8,6 +8,7 @@ mode = num1%num2
 quotient=num1/num2
 power=num1**num2
 
+print("All operations results")
 print('Sum of {0} and {1} is {2}'.format(num1,num2,sum))
 print('Difference of {0} and {1} is {2}'.format(num1,num2,diff)) 
 print('Product of {0} and {1} is {2}'.format(num1,num2,product))
